5f794b57692bd378ea21a968866cc689343f056d
Niraj Nale <132734839+nirajnale@users.noreply.github.com>
2026-07-28 12:19:02 +0530

    Time: 33 ms (89.75%) | Memory: 113.2 MB (60.69%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,43 @@
+class RandomizedSet {
+public:
+    vector<int> nums;
+    unordered_map<int, int> mp;
+
+    RandomizedSet() {
+
+    }
+
+    bool insert(int val) {
+
+        if (mp.count(val))
+            return false;
+
+        nums.push_back(val);
+        mp[val] = nums.size() - 1;
+
+        return true;
+    }
+
+    bool remove(int val) {
+
+        if (!mp.count(val))
+            return false;
+
+        int index = mp[val];
+        int last = nums.back();
+
+        nums[index] = last;
+        mp[last] = index;
+
+        nums.pop_back();
+        mp.erase(val);
+
+        return true;
+    }
+
+    int getRandom() {
+
+        int index = rand() % nums.size();
+        return nums[index];
+    }
+};
